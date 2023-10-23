1c210a8087b5deaf5ee1489a3f266e4e64291bf0
burnt eyed <68151125+abdullah-sami@users.noreply.github.com>
2023-10-23 18:55:53 +0600

    Update views.py

--- vercel_app/views.py
+++ vercel_app/views.py
@@ -1,5 +1,39 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index(request):
-    html = 'It\'s working'
-    return HttpResponse(html)
+
+    return render(request, 'index.html')
+
+
+
+def services(request):
+
+    return render(request, 'services.html')
+
+
+
+
+def team(request):
+
+    return render(request, 'team.html')
+
+
+
+
+
+def pricing(request):
+
+    return render(request, 'pricing.html')
+
+
+
+
+
+def contact(request):
+
+    return render(request, 'contact.html')
+
+
+
